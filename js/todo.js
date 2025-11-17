ea7e53c71324b35c1a3f28d5bc16187000da8746
ShawnYS-codemtl <syatsin@gmail.com>
2025-11-17 03:15:52 -0500

    feat: add checkbox to mark todo as complete

--- js/todo.js
+++ js/todo.js
@@ -18,19 +18,25 @@ export function init() {
     const input = document.getElementById("todo-input");
     const addBtn = document.getElementById("todo-add-btn");
 
-    // 3. Event listeners
-    addBtn.addEventListener("click", () => {
-        const value = input.value.trim()
-        if (value) {
-            todos.push(input.value)
-            save('todos', todos)
-        } else {
-            return
-        }
-        input.value = ''
-        render()
-    });
+    function addTodo() {
+        const text = input.value.trim();
+        if (!text) return;
 
+        todos.push({ text, completed: false });
+        save();
+        render();
+        input.value = "";
+    }
+
+    // 3. Event listeners
+    input.addEventListener("keydown", function (e) {
+        if (e.key === "Enter") {
+            addTodo()
+        }
+    })
+
+    addBtn.addEventListener("click", addTodo)
+        
     // Render todos on page load
     render()
 }
@@ -40,8 +46,11 @@ function render() {
     list.innerHTML = ''
     todos.forEach((todo, index) => {
         const li = document.createElement("li");
+        if (todo.completed){
+            li.classList.add('completed')
+        }
         const text = document.createElement("span")
-        text.textContent = todo
+        text.textContent = todo.text
 
         const delBtn = document.createElement("button")
         delBtn.textContent = 'x'
@@ -54,10 +63,19 @@ function render() {
             render();
         })
 
+        const checkbox = document.createElement('input')
+        checkbox.type = "checkbox"
+        checkbox.checked = todo.completed
+
+        checkbox.addEventListener("change", () => {
+            todo.completed = checkbox.checked
+            save("todos", todos)
+            render()
+        })
+
+        li.appendChild(checkbox);
         li.appendChild(text)
         li.appendChild(delBtn);
         list.appendChild(li)
-    })
-
-    
+    })    
 }
