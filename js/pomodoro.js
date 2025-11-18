e680882a6485d55ed73a9239826a3ddfcc2ce4f5
ShawnYS-codemtl <syatsin@gmail.com>
2025-11-17 20:47:43 -0500

    feat: pomodoro timer base functionality

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,28 +1,76 @@
-let interval = null;
-let timeLeft = 1500; // 25 min
+let intervalId = null;
+let duration = 1500
+let timeLeft = duration // 25 min
+
+const display = document.getElementById("timer-display");
+const startBtn = document.getElementById("start-btn");
+const pauseBtn = document.getElementById("pause-btn");
+const resetBtn = document.getElementById("reset-btn");
 
 export function init() {
     console.log("Pomodoro Module Loaded");
 
-    const display = document.getElementById("timer-display");
+    // Mode buttons (Pomodoro, Short break, Long break)
+    const modeBtns = document.querySelectorAll("[data-min]");
 
-    const startBtn = document.getElementById("start-btn");
-    const pauseBtn = document.getElementById("pause-btn");
-    const resetBtn = document.getElementById("reset-btn");
+    updateDisplay()
 
-    // TODO: Show initial time in display
+    startBtn.addEventListener("click", start);
 
-    startBtn.addEventListener("click", () => {
-        // TODO: Start interval (countdown)
-    });
+    pauseBtn.addEventListener("click", pause);
 
-    pauseBtn.addEventListener("click", () => {
-        // TODO: Pause timer
-    });
+    resetBtn.addEventListener("click", reset);
 
-    resetBtn.addEventListener("click", () => {
-        // TODO: Reset timer
+    modeBtns.forEach(btn => {
+        btn.addEventListener("click", () => {
+            const mins = Number(btn.dataset.min);
+            setMode(mins);
+        });
     });
 }
 
-// TODO: write helper to format time (mm:ss)
+function start(){
+    if (intervalId) return
+
+    intervalId = setInterval(() => {
+        timeLeft -= 1
+        if (timeLeft === 0){
+            clearInterval(intervalId)
+        }
+        updateDisplay()
+    }, 1000)
+}
+
+function pause(){
+    clearInterval(intervalId)
+    intervalId = null
+}
+
+function reset() {
+    timeLeft = duration
+    pause()
+    updateDisplay()
+}
+
+function updateDisplay() {
+    display.textContent = formatTime(timeLeft);
+}
+
+function formatTime(totalSeconds) {
+    const hours = Math.floor(totalSeconds / 3600)
+    const mins = Math.floor((totalSeconds % 3600) / 60)
+    const secs = totalSeconds % 60
+
+    const formattedHours = String(hours).padStart(2, '0')
+    const formattedMins = String(mins).padStart(2, '0')
+    const formattedSecs = String(secs).padStart(2, '0')
+
+    return formattedHours != '00' ? `${formattedHours}:${formattedMins}:${formattedSecs}` : `${formattedMins}:${formattedSecs}`
+}
+
+function setMode(mins) {
+    duration = mins * 60
+    timeLeft = duration
+    pause()
+    updateDisplay()
+}
